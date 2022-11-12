d4e5ea5eb2ee54830a20174486eded13e52c0064
MacBook <macbook@MacBook-Pro-MacBook.local>
2022-11-12 19:16:59 +0000

    create relevant tables

---
+++ backend/oxhack/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OxhackConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "oxhack"
---
+++ backend/oxhack/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Roles(models.Model):
+    role = models.CharField(max_length=100)
+    role_id = models.IntegerField()
+    q1_2021_nr_jobs = models.IntegerField()
+    q2_2021_nr_jobs = models.IntegerField()
+    q3_2021_nr_jobs = models.IntegerField()
+    q4_2021_nr_jobs = models.IntegerField()
+    q1_2022_nr_jobs = models.IntegerField()
+    q2_2022_nr_jobs = models.IntegerField()
+    q3_2022_nr_jobs = models.IntegerField()
+    q4_2022_nr_jobs = models.IntegerField()
+    salary = models.IntegerField()
+
+
+class Role_skills(models.Model):
+    role = models.CharField(max_length=100)
+    role_id = models.IntegerField()
+    skill = models.CharField(max_length=100)
+
+
+class Role_companies(models.Model):
+    role = models.CharField(max_length=100)
+    role_id = models.IntegerField()
+    company_name = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
